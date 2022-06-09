9d8c5290f0e58ccfa01e26324fa80632e4ee00bb
Mauregina <mauraregina@gmail.com>
2022-06-09 13:01:54 -0400

    add createUser feature
    Signed-off-by: Mauregina <mauraregina@gmail.com>

--- miniblog/src/hooks/useAuthentication.js
+++ miniblog/src/hooks/useAuthentication.js
@@ -1,11 +1,58 @@
+import { db } from '../firebase/config';
+
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    updateProfile
+} from "firebase/auth";
+
+import { useEffect, useState } from 'react';
+
 export const useAuthentication = () => {
-    const createUser = 'Usuário criado';
-    const error = 'deu erro';
-    const loading = 'carregando';
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [cancelled, setCancelled] = useState(false);
+
+    const auth = getAuth();
+
+    // cleanup - deal with memory leak
+    const isCancelled = () => {
+        if (cancelled) return;
+    } 
+
+    const createUser = async (data) => {
+        isCancelled();
+        setError(null);
+        setLoading(true);
+
+        try {
+            const { user } = await createUserWithEmailAndPassword(
+                auth,
+                data.email,
+                data.password
+            )
+
+            await updateProfile( user, {
+                displayName: data.name,
+            } );
+
+            setLoading(false);
+            return user;
+        }
+        catch (error) {
+            setError(error.message);
+        }
+
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        return () => setCancelled(true);
+    }, []);
 
     return {
         createUser,
-        error,
         loading,
+        error
     };
 };
--- miniblog/src/pages/Register/Register.js
+++ miniblog/src/pages/Register/Register.js
@@ -1,24 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import styles from './Register.module.css';
 
 import {useAuthentication} from '../../hooks/useAuthentication';
 
 const Register = () => {
-  const [name, setName] = useState();  
-  const [email, setEmail] = useState();  
-  const [password, setPassword] = useState();  
-  const [confirmPassword, setConfirmPassword] = useState();  
-  const [error, setError] = useState();
-
-  const { createUser, error: authError, loading } = useAuthentication();
-
-  console.log(createUser);
-  console.log(authError);
-  console.log(loading);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const { createUser, loading, error: authError } = useAuthentication();
 
   const handleSubmit = (e) => {
       e.preventDefault();
+
       setError("");
       
       if (password != confirmPassword) {
@@ -29,11 +25,20 @@ const Register = () => {
       const user = {
           name,
           email,
-          password 
+          password
       }
 
-      console.log(user);
-  }  
+      const res = createUser(user);
+      console.log('RETORNO -> ', res);
+  };
+  
+  useEffect(() => {
+    setError(authError);
+  }, [authError]);
+
+  const cleanError = () => {
+      setError("");
+  }
 
   return (
     <div className={styles.register}>
@@ -42,21 +47,38 @@ const Register = () => {
         <form onSubmit={handleSubmit}>
             <label>
                 <span>Name</span>
-                <input type="text" name='name' placeholder='Type your name' required onChange={(e) => setName(e.target.value)}/>
+                <input type="text" name='name' placeholder='Type your name' required 
+                onChange={(e) => { setName(e.target.value); 
+                                   cleanError();
+                                 }}/>
             </label>    
             <label>
                 <span>Email</span>
-                <input type="text" name='email' placeholder='Type your email' required onChange={(e) => setEmail(e.target.value)}/>
+                <input type="text" name='email' placeholder='Type your email' required 
+                onChange={(e) => { setEmail(e.target.value); 
+                                   cleanError();
+                                 }}/>
             </label>   
             <label>
                 <span>Password</span>
-                <input type="text" name='password' placeholder='Type your password' required onChange={(e) => setPassword(e.target.value)}/>
+                <input type="text" name='password' placeholder='Type your password' required
+                onChange={(e) => { setPassword(e.target.value); 
+                                   cleanError();
+                                 }}/>
             </label>   
             <label>
                 <span>Confirm Password</span>
-                <input type="text" name='confirmPassword' placeholder='Type your password again' required onChange={(e) => setConfirmPassword(e.target.value)}/>
-            </label>   
-            <button className='btn'>Register</button>
+                <input type="text" name='confirmPassword' placeholder='Type your password again' required
+                onChange={(e) => { setConfirmPassword(e.target.value); 
+                                   cleanError();
+                                 }}/>
+            </label>
+            {loading &&   
+                <button className='btn' disabled>Loading</button>
+            }
+            {!loading &&   
+                <button className='btn'>Register</button>
+            }
             {error &&
                 <p className='error'>{error}</p>
             }
